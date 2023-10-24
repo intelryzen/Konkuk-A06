ede4c96e527ce3f7fd75f75a2dfd6812a7b091c3
suhan.song <hoxz12012@gmail.com>
2023-10-24 13:02:34 +0900

    데이터 파일 역직렬화

--- src/main.py
+++ src/main.py
@@ -10,15 +10,14 @@ stockDict = {}
 
 
 def main():
-<<<<<<< HEAD
     basket = ShoppingBasket()
     value = chooseMenu()
     if value == 5:
         payment(basket)
     # test()
     # print("시작")
-=======
     getStockDict()
+    getFoodList()
 
 # class FileIntegrityChecker:
 
@@ -31,6 +30,36 @@ class Food:
         self.recipe = recipe
         #최초에는 0으로 초기화
         self.orderable = 0
+
+def getFoodList():
+    try:
+        foodTxt = open(foodFilePath, 'r', encoding='UTF8')
+    except:
+        raise MyCustomError("파일 읽기에 실패했습니다.")
+    line = foodTxt.readlines()
+    for food in line:
+        data = food.strip().split('\t')
+        if (len(data) == 5):
+            try:
+                #정수값으로 정의된것들 확인
+                foodNo = int(data[0])
+                foodTypeNo = int(data[1])
+                price = int(data[3])
+                #음식명
+                name = data[2]
+                #str -> dic
+                recipe = eval(data[4])
+
+                foodList.append(Food(foodNo, foodTypeNo, name, price, recipe))
+
+            except:
+                raise MyCustomError("데이터 타입을 확인해 주세요")
+        else:
+            raise MyCustomError("파일 형식에 어긋남")
+
+    # 확인용
+    print(foodList[3].recipe)
+
 
 def getStockDict():
     try:
@@ -57,31 +86,10 @@ def getStockDict():
     # 확인용
     print(stockDict)
 
-
-# def getFoodList():
-#     try:
-#         foodTxt = open(foodFilePath, 'r', encoding='UTF8')
-#     except:
-#         raise MyCustomError("파일 읽기에 실패했습니다.")
-#     line = foodTxt.readlines()
-#     for food in line:
-#         data = food.strip().split('\t')
-#         if (len(data) == 5):
-#             try:
-#
-#             except:
-#                 raise MyCustomError("재고 번호나 수량이 정수값이 아닙니다.")
-#         else:
-#             raise MyCustomError("파일 형식에 어긋남")
-
-
-
 class MyCustomError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
-
->>>>>>> af64ab10fba3ed702bd03d9c21fbe7b8da61236e
 
 
 if __name__ == "__main__":
