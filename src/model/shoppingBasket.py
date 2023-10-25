9f31843cdd07046fea5853f8f5d8b0b536260d9b
우승식 <intelryzen@naver.com>
2023-10-25 23:33:08 +0900

    기능 추가 show , add, remove(요구사항 분석) (원규연)
    버그 수정

--- src/model/shoppingBasket.py
+++ src/model/shoppingBasket.py
@@ -1,24 +1,40 @@
+from res import foodList
+
+
 class ShoppingBasket:
+    """
+    여기에 ShoppingBasket 클래스 정의
+    """
+
     def __init__(self):
-        self.basket = []  # [음식, 수량]을 원소로 갖는 리스트
-        self.totalPrice = 0  # basket에 들어있는 음식의 총 가격합
+        self.basket = []  # [음식번호, 수량]을 원소로 가지는 리스트
+        self.totalPrice = 0  # basket에 들어있는 음식의 총 가격합임. 값이 0 이면 장바구니가 비었음을 의미함.
 
     # 장바구니 담은 목록 출력
-    def showBasket(self):
-        from main import foodList
+    def show(self):
         print("장바구니:")
         for item in self.basket:
-            food = next(
+            selectedFood = next(
                 (food for food in foodList if food.no == item[0]), None)
-            if food:
-                print(f"{food.name}: {item[1]}")
+            if selectedFood:
+                print(f"{selectedFood.name}: {item[1]}")
 
-    # def addItem(self, food, quantity, price):
-    #     """
-    #     항목을 basket에 추가하고 totalPrice를 업데이트한다.
-    #     food : 음식 이름 혹은 번호
-    #     quantity : 음식 수량
-    #     price : 음식 단가
-    #     """
-    #     self.basket.append([food, quantity])
-    #     self.totalPrice += price * quantity
+    def add(self, food, quantity):
+        """
+        장바구니에 음식을 추가함
+        """
+        # basket에 이미 food.no 을 담았는지 찾는다.
+        for item in self.basket:
+            if item[0] == food.no:
+                item[1] += quantity
+                break
+        else:  # for-else 구문을 사용하여 for 문이 끝까지 실행되면 새롭게 아이템을 추가함.
+            self.basket.append([food.no, quantity])
+        self.totalPrice += food.price * quantity
+
+    def remove(self, food, quantity):
+        """
+        장바구니에서 음식을 삭제함
+        """
+        self.basket.remove([food.no, quantity])
+        self.totalPrice -= food.price * quantity
