67f8dfd1ce5016be0896d7940a85a0c6281407a3
우승식 <intelryzen@naver.com>
2023-10-25 23:25:25 +0900

    [오류] + {default_message} 수정

--- src/model/customError.py
+++ src/model/customError.py
@@ -1,4 +1,4 @@
 class MyCustomError(Exception):
     def __init__(self, message):
         self.message = message
-        super().__init__(self.message)
+        super().__init__(f"[오류] {self.message}")
