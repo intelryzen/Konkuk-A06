b86e88b030bc03cc4f3e98fe387f1465cb7ade6b
우승식 <intelryzen@naver.com>
2023-10-25 23:24:25 +0900

    - 기능추가 요구사항분석 관련 기능 (고영빈)
    - 수정한 것
    getFileList, getStockList => clear() 추가, 'x 번째 줄' 오류 문구 추가출력
    stockDict = originStockDict 수정
    
    - 사라진 것
    기존 파일유효성체크 클래스 => getFileList, getStockList 대신 수행

--- src/main.py
+++ src/main.py
@@ -1,100 +1,136 @@
-import copy
-from prompt import *
+import sys
+from res import *
 from model import *
+from prompt import *
+import copy
 
-# 전역 변수
-foodFilePath, stockFilePath = '../files/food.txt', '../files/stock.txt'
-foodType = {0: '메인메뉴', 1: '사이드메뉴', 2: '음료수'}
-foodList = []
-stockDict = {}
-
-
-def main():
-    basket = ShoppingBasket()
-    value = chooseMenu()
-    if value == 5:
-        payment(basket)
-    # test()
-    # print("시작")
-    getStockDict()
-    getFoodList()
-
-# class FileIntegrityChecker:
-
-class Food:
-    def __init__(self, no, foodTypeNo, name, price, recipe):
-        self.no = no
-        self.foodTypeNo = foodTypeNo
-        self.name = name
-        self.price = price
-        self.recipe = recipe
-        #최초에는 0으로 초기화
-        self.orderable = 0
 
 def getFoodList():
+    global foodList
+    # 결제 후 다시 호출될 수 있어서 list 를 항상 비워주어야 함
+    foodList.clear()
     try:
         foodTxt = open(foodFilePath, 'r', encoding='UTF8')
     except:
         raise MyCustomError("파일 읽기에 실패했습니다.")
     line = foodTxt.readlines()
-    for food in line:
+    for line_number, food in enumerate(line):
         data = food.strip().split('\t')
         if (len(data) == 5):
             try:
-                #정수값으로 정의된것들 확인
+                # 정수값으로 정의된것들 확인
                 foodNo = int(data[0])
                 foodTypeNo = int(data[1])
                 price = int(data[3])
-                #음식명
+                # 음식명
                 name = data[2]
-                #str -> dic
+                # str -> dic
                 recipe = eval(data[4])
 
                 foodList.append(Food(foodNo, foodTypeNo, name, price, recipe))
 
             except:
-                raise MyCustomError("데이터 타입을 확인해 주세요")
+                raise MyCustomError(
+                    f"{foodFilePath} {line_number+1}번째줄 데이터 타입을 확인해 주세요")
         else:
-            raise MyCustomError("파일 형식에 어긋남")
+            raise MyCustomError(f"{foodFilePath} {line_number+1}번째줄 형식이 어긋남")
 
-    # 확인용
-    print(foodList[3].recipe)
+    # # 확인용
+    # print(foodList[3].recipe)
 
     foodTxt.close()
 
 
 def getStockDict():
+    global stockDict
+    # 결제 후 다시 호출될 수 있어서 stockDict 를 항상 비워주어야 함
+    stockDict.clear()
     try:
         stockTxt = open(stockFilePath, 'r', encoding='UTF8')
     except:
         raise MyCustomError("파일 읽기에 실패했습니다.")
 
     line = stockTxt.readlines()
-    for stock in line:
+    for line_number, stock in enumerate(line):
         data = stock.strip().split('\t')
         if (len(data) == 3):
             try:
-                #재고 번호
+                # 재고 번호
                 stockNo = int(data[0])
-                #재고 갯수
+                # 재고 갯수
                 stockCnt = int(data[2])
 
                 stockDict[stockNo] = stockCnt
             except:
-                raise MyCustomError("재고 번호나 수량이 정수값이 아닙니다.")
+                raise MyCustomError(
+                    f"{stockFilePath} {line_number+1}번째줄 재고 번호나 수량이 정수값이 아닙니다.")
         else:
-            raise MyCustomError("파일 형식에 어긋남")
+            raise MyCustomError(
+                f"{stockFilePath} {line_number+1}번째줄 형식이 어긋남")
 
     # 확인용
-    print(stockDict)
+    # print(stockDict)
 
     stockTxt.close()
 
 
-class MyCustomError(Exception):
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
+def exit():
+    sys.exit()
+
+
+def main():
+    global foodList
+    global stockDict
+    while True:
+        try:
+            getFoodList()  # food 역직렬화
+            getStockDict()  # stock 역직렬화
+            originStockDict = copy.deepcopy(stockDict)  # 깊은 복사
+        except Exception as e:
+            print(e)  # 파일이 무효하면 프로그램 종료
+            exit()
+
+        # while True:
+        #     date = getDate()
+        #     if (checkDate(date) == 1):  # 올바른 날짜 입력시까지 입력 계속 받음
+        #         break
+        # user_id = input()  # 유저아이디 입력
+
+        while True:  # 모드 프롬프트
+            basket = ShoppingBasket()  # user_id, date로 인자로 basket 생성
+
+            ret1 = chooseMode()
+            if ret1 == 2:  # 종료 선택시
+                exit()
+
+            while True:  # 주문 프롬프트
+                ret2 = chooseMenu()
+                if ret2 == 0:  # 뒤로가기 선택시
+                    # stockDict 원래대로 복구
+                    stockDict.clear()
+                    stockDict.update(originStockDict)
+                    break
+                elif ret2 == 5:  # 결제하기 선택시
+                    ret4 = payment(basket)
+                    if ret4 == 1:  # 결제 완료되면
+                        # updateOrderFile()  # 주문 파일 업데이트
+                        # updateStockFile()  # 재고 파일 업데이트
+                        main()  # main 재귀호출
+                        exit()
+                elif ret2 == 4:  # 장바구니 수정시
+                    print("미구현")
+                #     while True:
+                #         ret5 == showBasket()
+                #         modifyBasket(ret5)  # 장바구니 담을때 주문가능 개수가 변동되는 경우 복구까지
+                #         if ret5 == 0:
+                #             break
+
+                else:  # ret2 == 1, 2, or 3
+                    while True:  # 장바구니 프롬프트
+                        updateFoodList()  # 주문 가능 음식 개수 수정
+                        ret3 = insertBasket(ret2, basket)
+                        if ret3 == 1:  # 뒤로가기 또는 주문 가능 음식없으면
+                            break
 
 
 if __name__ == "__main__":
