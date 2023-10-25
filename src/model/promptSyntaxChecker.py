5e9c10cba0b9d869146f650a4ffd27b94df03e26
우승식 <intelryzen@naver.com>
2023-10-25 23:30:25 +0900

    텍스트 수정, split \t 수정

--- src/model/promptSyntaxChecker.py
+++ src/model/promptSyntaxChecker.py
@@ -12,16 +12,16 @@ class PromptSyntaxChecker:
             # 문법에 맞지 않으면 MyCustomError를 발생시킬 수 있음
             return value
         except ValueError:
-            raise MyCustomError(f"[오류] 문법에 맞지 않은 입력입니다.")
+            raise MyCustomError(f"문법에 맞지 않은 입력입니다.")
 
     def checkBasketSyntax(self):
         try:
             if self.inputValue == '0':  # 뒤로가기의 경우
                 return 0
 
-            foodNumber, quantity = map(int, self.inputValue.split())
+            foodNumber, quantity = map(int, self.inputValue.split('\t'))
             # 예외적인 경우나 입력값의 범위 등을 여기서 확인하고,
             # 문법에 맞지 않으면 MyCustomError를 발생시킬 수 있음
             return [foodNumber, quantity]
         except ValueError or IndexError:
-            raise MyCustomError(f"Invalid syntax: {self.inputValue}")
+            raise MyCustomError(f"입력 오류입니다. '0' 또는 '음식번호↹수량' 을 입력하세요")
