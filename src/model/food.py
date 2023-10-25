3beb4d86a351ab4348d02b9a8090499e8798f61d
우승식 <intelryzen@naver.com>
2023-10-25 23:27:54 +0900

    __str__ 추가 orderable = 0 기본값 변경 (수한)

--- src/model/food.py
+++ src/model/food.py
@@ -1,12 +1,11 @@
 class Food:
-    def __init__(self, no, foodTypeNo, name, price, recipe, orderable):
+    def __init__(self, no, foodTypeNo, name, price, recipe):
         self.no = no  # 음식 고유번호
         self.foodTypeNo = foodTypeNo  # foodType을 참조하는 번호
         self.name = name  # 음식 이름
         self.price = price  # 음식 가격
         self.recipe = recipe  # stock의 재고번호와 갯수로 구성된 딕셔너리
-        self.orderable = orderable  # 주문 가능한 갯수
+        self.orderable = 0  # 주문 가능한 갯수
 
-    # 설계서에 없음.
-    # def __str__(self):
-    #     return f"음식 이름: {self.name}, 가격: {self.price}원, 주문 가능한 갯수: {self.orderable}"
+    def __str__(self):
+        return f"{self.name}: ₩{self.price} (가능 수량: {self.orderable})"
