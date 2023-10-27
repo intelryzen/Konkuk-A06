99523931914917f8c81082783bec9e7e5a10b0ac
우승식 <intelryzen@naver.com>
2023-10-27 18:42:08 +0900

    no message

--- src/res.py
+++
@@ -1,5 +0,0 @@
-# 전역 변수
-foodFilePath, stockFilePath, orderFilePath = '../files/food.txt', '../files/stock.txt', '../files/order.txt'
-foodType = {0: '메인메뉴', 1: '사이드메뉴', 2: '음료수'}
-foodList = []
-stockDict = {}
