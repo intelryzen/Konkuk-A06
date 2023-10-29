479626c73992e2696030ec536e2b17f7853a2259
우승식 <intelryzen@naver.com>
2023-10-29 18:45:56 +0900

    양끝 공백 오류 추가

--- src/model/promptSyntaxChecker.py
+++ src/model/promptSyntaxChecker.py
@@ -19,6 +19,10 @@ class PromptSyntaxChecker:
             if self.inputValue == '0':  # 뒤로가기의 경우
                 return 0
 
+            # 입력의 양 끝에 공백 허용 안함.
+            if self.inputValue.startswith(' ') or self.inputValue.endswith(' '):
+                raise ValueError()
+
             foodNumber, quantity = map(int, self.inputValue.split('\t'))
             # 예외적인 경우나 입력값의 범위 등을 여기서 확인하고,
             # 문법에 맞지 않으면 MyCustomError를 발생시킬 수 있음
